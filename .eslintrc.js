b65b810cdfd115ae67bd10ad29da1cd985cbbb75
eugenioclrc <eugenioclrc@gmail.com>
2021-11-13 16:20:20 -0300

    module refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-    'env': {
-        'commonjs': true,
-        'es2021': true,
-        'node': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 13
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 13
+  },
+  'rules': {
+    'indent': [
+      'error',
+      4
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
---
+++ modules/module.supply.js
@@ -0,0 +1,48 @@
+const parser = require('@solidity-parser/parser');
+
+const chalk = require('chalk');
+
+
+module.exports = function run(ast) {
+    const ret = {
+        log: [],
+        findings: []
+    };
+    let uses_totalSupply = false;
+    let has_totalSupply = false;
+
+    parser.visit(ast, {
+        StateVariableDeclaration(node, parentNode){
+            // find _totalSupply declaration probably can be done better
+            if(parentNode.kind == 'contract'){
+                for (let v of node.variables) {
+                    if (v.name == '_totalSupply') {
+                        uses_totalSupply = true;
+                        break;
+                    }                        
+                }
+            }
+        },
+        FunctionDefinition(node, parentNode) {
+            // search for totalSupply getter. to be removed, is useless
+            if (node.name == 'totalSupply' && parentNode.kind== 'contract'){
+                for (let s of node.body.statements){
+                    if (s.expression.name == '_totalSupply'){
+                        has_totalSupply = true;
+                        break;
+                    }
+                }
+            }
+        }
+    });
+
+    if(uses_totalSupply) {
+        ret.logs.push(chalk.green('Uses uint256 _totalSupply'));
+    }
+    if(has_totalSupply) {
+        ret.logs.push(chalk.green('Uses _totalSupply'));
+    } else {
+        ret.findings.push(chalk.red('[Not using _totalSupply" possible hidden mint'));
+    }
+    return ret;
+};
